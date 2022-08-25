bfca2076f69369a9d31300fd4e738e792fafb99c
Konrad Siwczyński <Konrad_Siwczynski@rossmann.com.pl>
2022-08-25 14:30:14 +0200

    zadania 1 i 2

---
+++ Loans.Tests/LoanTermShould.cs
@@ -0,0 +1,27 @@
+﻿using Loans.Domain.Applications;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loans.Tests
+{
+	public class LoanTermShould
+	{
+		[Test]
+		public void LoanTermMonthsInTermTest()
+		{
+			var sut = new LoanTerm(1);
+
+			Assert.That(sut.ToMonths(), Is.EqualTo(12));
+		}
+
+		[Test]
+		public void LoanTermYearTest()
+		{
+			var sut = new LoanTerm(1);
+
+			Assert.That(sut.Years, Is.EqualTo(1));
+		}
+	}
+}
